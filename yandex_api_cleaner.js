d27404d879656f7d834ce6e584347b45feb99b6c
Krest-Neva <KRMotya@icloud.com>
2026-03-26 06:50:22 +0300

    Update yandex_api_cleaner.js

--- yandex_api_cleaner.js
+++ yandex_api_cleaner.js
@@ -1,38 +1,35 @@
 let body = $response.body;
-const url = $request.url;
 if (body) {
-try {
-let obj = JSON.parse(body);
-let modified = false;
-const adKeys = ['ads','adverts','banners','badv','promoted','native_ads','direct','sponsor'];
-if (obj && typeof obj === 'object' && !Array.isArray(obj)) {
-adKeys.forEach(key => {
-if (obj[key] !== undefined) { delete obj[key]; modified = true; }
-});
-}
-function removeAdsFromArray(arr) {
-if (!Array.isArray(arr)) return arr;
-const initialLength = arr.length;
-const filtered = arr.filter(item => {
-if (!item || typeof item !== 'object') return true;
-return !(item.type === 'ad' || item.type === 'banner' || item.is_ad === true || item.is_promoted === true || item.is_sponsor === true || item.ad_info);
-});
-if (filtered.length !== initialLength) modified = true;
-return filtered;
-}
-['data','items','results','feed','toponyms'].forEach(key => {
-if (obj[key] && Array.isArray(obj[key])) {
-obj[key] = removeAdsFromArray(obj[key]);
-}
-});
-if (obj.fact?.badv) { delete obj.fact.badv; modified = true; }
-if (obj.forecast?.badv) { delete obj.forecast.badv; modified = true; }
-if (obj.searchResult?.results) {
-obj.searchResult.results = removeAdsFromArray(obj.searchResult.results);
-}
-if (modified) body = JSON.stringify(obj);
-} catch (e) {
-console.log("Yandex API Cleaner Error: " + e);
-}
+    try {
+        let obj = JSON.parse(body);
+        let modified = false;
+        const adPatterns = /^(ad|ads|advert|banner|promo|direct|sponsor|metrica|statistics|analytics|survey)/i;
+        function scanner(node) {
+            if (!node || typeof node !== 'object') return node;
+            if (Array.isArray(node)) {
+                return node.filter(item => {
+                    if (item && typeof item === 'object') {
+                        const isAd = item.type?.toString().match(adPatterns) || 
+                                     item.layout?.toString().match(adPatterns) || 
+                                     item.is_ad === true || item.ad_info || item.is_promoted === true;
+                        if (isAd) { modified = true; return false; }
+                        scanner(item);
+                    }
+                    return true;
+                });
+            }
+            Object.keys(node).forEach(key => {
+                if (key.match(adPatterns)) {
+                    delete node[key];
+                    modified = true;
+                } else if (typeof node[key] === 'object') {
+                    scanner(node[key]);
+                }
+            });
+            return node;
+        }
+        obj = scanner(obj);
+        if (modified) body = JSON.stringify(obj);
+    } catch (e) {}
 }
 $done({ body });
