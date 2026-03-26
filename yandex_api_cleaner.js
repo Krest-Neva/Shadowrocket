bd87ea4ff8372cc75101fd1affd7a4169f93738f
Krest-Neva <KRMotya@icloud.com>
2026-03-26 07:17:28 +0300

    Update yandex_api_cleaner.js

--- yandex_api_cleaner.js
+++ yandex_api_cleaner.js
@@ -3,27 +3,37 @@ if (body) {
     try {
         let obj = JSON.parse(body);
         let modified = false;
-        const adPatterns = /^(ad|ads|advert|banner|promo|direct|sponsor|metrica|statistics|analytics|survey)/i;
+        const adPatterns = /^(?:ad|ads|advert|banner|promo|direct|sponsor|sponsored|metrica|statistics|analytics|survey|widget|tracking)/i;
+        const adPattern2 = /@monetize|UrbanAds|HorizontalIncut|Madv|MediaCarousel|searchIncut/i;
         function scanner(node) {
             if (!node || typeof node !== 'object') return node;
             if (Array.isArray(node)) {
                 return node.filter(item => {
                     if (item && typeof item === 'object') {
-                        const isAd = item.type?.toString().match(adPatterns) || 
-                                     item.layout?.toString().match(adPatterns) || 
-                                     item.is_ad === true || item.ad_info || item.is_promoted === true;
-                        if (isAd) { modified = true; return false; }
+                        // Удаляем объект, если явно рекламный
+                        if (item.is_ad === true || item.is_promoted === true ||
+                            adPattern2.test(item.widgetName) || adPattern2.test(item.apiaryWidgetName) ||
+                            item.dataAuto === 'searchIncut' || item.dataZoneData?.bannerUrl) {
+                            modified = true;
+                            return false;
+                        }
                         scanner(item);
                     }
                     return true;
                 });
             }
             Object.keys(node).forEach(key => {
-                if (key.match(adPatterns)) {
+                if (adPatterns.test(key)) {
                     delete node[key];
                     modified = true;
-                } else if (typeof node[key] === 'object') {
-                    scanner(node[key]);
+                } else {
+                    // Например, если поле widgetName содержит рекламу
+                    if ((key === 'widgetName' || key === 'apiaryWidgetName') && adPattern2.test(node[key])) {
+                        delete node[key];
+                        modified = true;
+                    } else {
+                        scanner(node[key]);
+                    }
                 }
             });
             return node;
