05e0ad2cb704538d1d35980a093a987b5d704c6b
Krest-Neva <KRMotya@icloud.com>
2026-04-12 00:42:05 +0300

    Update Test_Script.js

--- Scripts/Test_Script.js
+++ Scripts/Test_Script.js
@@ -1,2 +1,14 @@
-console.log("!!! SCRIPT FIRED !!!"); // Это сообщение мы будем искать в логах
-$done({ policy: "REJECT" });
+// Оставляем след в логах
+console.log("---SCRIPT_START---");
+
+// Собираем всю доступную информацию о запросе в одну строку
+let requestInfo = "Script triggered!";
+if ($request) {
+    requestInfo = requestInfo + " URL: " + $request.url;
+}
+
+// Выводим информацию в лог
+console.log(requestInfo);
+
+// Завершаем скрипт, НЕ изменяя политику
+$done({});
