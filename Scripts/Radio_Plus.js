0b677625ea27802c6122858811ece0bb4ed2e28b
Krest-Neva <KRMotya@icloud.com>
2026-07-07 20:50:23 +0300

    Update Radio_Plus.js

--- Scripts/Radio_Plus.js
+++ Scripts/Radio_Plus.js
@@ -2,16 +2,37 @@ let body = $response.body;
 try {
     let obj = JSON.parse(body);
     console.log("Radio_Plus: Original = " + body);
-    if (obj && typeof obj === 'object' && obj.hasOwnProperty('isAvailable')) {
-        obj.isAvailable = true;
-        let newBody = JSON.stringify(obj);
-        console.log("Radio_Plus: Modified = " + newBody);
-        $done({body: newBody});
-    } else {
-        console.log("Radio_Plus: No 'isAvailable' field, passing through");
-        $done({body: body});
+    function updateKeys(obj) {
+        if (obj && typeof obj === 'object') {
+            for (let key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    if (key === 'isAvailable' || key === 'premium' || key === 'isPremium' || key === 'subscribed' || key === 'active') {
+                        if (typeof obj[key] === 'boolean') obj[key] = true;
+                        else if (typeof obj[key] === 'string') obj[key] = "true";
+                        else if (typeof obj[key] === 'number') obj[key] = 1;
+                    }
+                    if (typeof obj[key] === 'object') {
+                        updateKeys(obj[key]);
+                    }
+                }
+            }
+        }
     }
+    updateKeys(obj);
+    if (body.includes('"products"')) {
+        if (!obj.products || obj.products.length === 0) {
+            obj.products = [{ id: "premium", name: "Premium", price: 0 }];
+        }
+    }
+    if (body.includes('"active_products"') || body.includes('"activeProducts"')) {
+        if (!obj.active_products || obj.active_products.length === 0) {
+            obj.active_products = [{ productId: "premium", expires: "2099-01-01" }];
+        }
+    }
+    let newBody = JSON.stringify(obj);
+    console.log("Radio_Plus: Modified = " + newBody);
+    $done({ body: newBody });
 } catch (e) {
     console.log("Radio_Plus: Error = " + e);
-    $done({body: body});
+    $done({ body: body });
 }
