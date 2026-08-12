843859b3d47fa7fd9bc80bc992c429b81480d78b
Krest-Neva <KRMotya@icloud.com>
2026-08-12 18:23:28 +0300

    Modify response body for IP restriction message

---
+++ Scripts/Avito.js
@@ -0,0 +1,4 @@
+var body = $response.body;
+body = body.replace(/Доступ ограничен: проблема с IP/g, 'Бан по причине пидорас');
+body = body.replace(/<title>Доступ ограничен: проблема с IP<\/title>/g, '<title>Бан по причине пидорас</title>');
+$done({ body });
