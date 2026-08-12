4b19fdcd3e1f12380ce427e1bc593172ad3d637d
Krest-Neva <KRMotya@icloud.com>
2026-08-12 18:33:53 +0300

    Update Avito.js

--- Scripts/Avito.js
+++ Scripts/Avito.js
@@ -1,13 +1,9 @@
-if (typeof $response !== 'undefined' && $response.body) {
+if (typeof $response !== 'undefined' && $response.body && $response.status == 403) {
     try {
         let body = $response.body;
-        if (body.includes('Доступ ограничен: проблема с IP')) {
-            body = body.replace(/Доступ ограничен: проблема с IP/g, 'Бан по причине пидорас');
-            body = body.replace(/<title>Доступ ограничен: проблема с IP<\/title>/g, '<title>Бан по причине пидорас</title>');
-            $done({ body: body });
-        } else {
-            $done({});
-        }
+        body = body.replace(/Доступ ограничен: проблема с IP/g, 'Бан по причине пидорас');
+        body = body.replace(/<title>Доступ ограничен: проблема с IP<\/title>/g, '<title>Бан по причине пидорас</title>');
+        $done({ body });
     } catch (e) {
         $done({});
     }
