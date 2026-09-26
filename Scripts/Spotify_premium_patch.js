69b8aaa6944759586bdd6862c362d9f514831a1b
Krest-Neva <KRMotya@icloud.com>
2026-09-26 12:18:41 +0300

    Clear loudness levels in account attributes
    Reset loudness levels to an empty string.

--- Scripts/Spotify_premium_patch.js
+++ Scripts/Spotify_premium_patch.js
@@ -60,7 +60,7 @@ function processMapObj(accountAttributesMapObj){
     accountAttributesMapObj['ads'] = {boolValue : false};
     accountAttributesMapObj['on-demand'] = {boolValue : true};
     accountAttributesMapObj['name'] = {stringValue : 'Spotify Premium'};
-    accountAttributesMapObj['loudness-levels'] = {stringValue : '1:-5.0,0.0,3.0:-2.0'};
+    accountAttributesMapObj['loudness-levels'] = {stringValue: ''};
     accountAttributesMapObj['product-expiry'] = {stringValue : endDateStr};
     accountAttributesMapObj['social-session'] = {boolValue : true};
     accountAttributesMapObj['pick-and-shuffle'] = {boolValue : false};
